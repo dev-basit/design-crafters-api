64c17f8ad758fc5c26c3431c71fc783a73745c23
Abdul Basit <dev.basit2020@gmail.com>
2023-08-29 05:24:42 +0500

    alphanumeric password valdiation added

--- models/user.js
+++ models/user.js
@@ -54,7 +54,7 @@ function validateUser(user) {
   const schema = {
     name: Joi.string().min(3).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(4).max(1024).required(),
+    password: Joi.string().alphanum().min(8).required(),
     userType: Joi.string().valid("buyer", "seller").required(),
     profilePicture: Joi.string().required(),
   };
